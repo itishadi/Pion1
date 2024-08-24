5d1229cb87e05dd394c9c6d78354d091db944d18
itishadi <113441103+itishadi@users.noreply.github.com>
2024-08-24 17:51:38 +0200

    saving

---
+++ Pion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
